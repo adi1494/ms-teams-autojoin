5f0b46aa806f1bcc75700018be989be88772f5e0
Ishaan Aditya <ishaanaditya.v@gmail.com>
2021-09-10 13:04:50 +0530

    fix: bugs preventing joining of class

--- ms-teams-autojoin.py
+++ ms-teams-autojoin.py
@@ -1,3 +1,4 @@
+from logging import log
 import time
 import dotenv
 import datetime as dt
@@ -32,18 +33,24 @@ from modules.util import (
 
 
 def leave_meeting(team_name: str, class_time: str) -> None:
-    logger.info("sleeping for 30 mins")
-    time.sleep(config.HALF_HOUR)
+    # logger.info("sleeping for 30 mins")
+    # time.sleep(config.HALF_HOUR)
+    
+    logger.info("sleeping for 10 secs")
+    time.sleep(10)
 
     def leave() -> None:
         """leave the current meeting"""
-        browser.find_element_by_css_selector(
-            "button#app-bar-2a84919f-59d8-4441-a975-2a8c2643b741"
-        ).click()
-        time.sleep(1)
-        browser.find_element_by_css_selector("button#hangup-button").click()
-        logger.info(f"left {team_name} meeting")
-
+        try:
+            browser.find_element_by_css_selector("button#app-bar-2a84919f-59d8-4441-a975-2a8c2643b741").click()
+            time.sleep(1)
+            browser.find_element_by_css_selector("button#hangup-button").click()
+            logger.info(f"left {team_name} meeting")
+        
+        except Exception as e:
+            logger.debug(f"this should not have happened...")
+            logger.error(f"some exception raised while trying to leave meeting:\n{e}")    
+        
     # TODO needs work
     def leave_on_condition() -> None:
         participants_button = browser.find_element_by_css_selector("button#roster-button")
@@ -57,18 +64,14 @@ def leave_meeting(team_name: str, class_time: str) -> None:
             return elements
 
         def attendee_elem_to_num(element: WebElement) -> int:
+            """ helper function to enable sorting/max of attendee_element list """
             str_num = element.get_attribute("innerHTML")[1:-1]
             return int(str_num)
 
         while True:
             elements = check_for_attendees()
             max_num = max(elements, key=attendee_elem_to_num)
-
-            string = browser.find_element_by_css_selector("span.toggle-number").get_attribute(
-                "innerHTML"
-            )
-            number = int(string[1:-1])
-            # print('this is the max_num '+str(max_num))
+            max_num = attendee_elem_to_num(max_num)
 
             if max_num > 20 and get_time_difference(class_time) < config.ONE_HOUR:
                 logger.info(f"conditions not met, participants: {max_num}")
@@ -92,7 +95,7 @@ def leave_meeting(team_name: str, class_time: str) -> None:
 
 
 def join_class(team_name: str, class_time: str) -> None:
-    def wait_for_team() -> None:
+    def wait_for_team(team_name) -> None:
         """custom wait times for each subject"""
         wait_dict = {
             "IE": 300,
@@ -102,7 +105,8 @@ def join_class(team_name: str, class_time: str) -> None:
             "DM": 30,
             "PM": 30,
         }
-        time_to_wait = wait_dict.get(team_name)
+        # time_to_wait = wait_dict.get(team_name)
+        time_to_wait = 0
         logger.info(f"sleeping for {time_to_wait} seconds for {team_name} class")
         time.sleep(time_to_wait)
 
@@ -116,9 +120,8 @@ def join_class(team_name: str, class_time: str) -> None:
         nonlocal team_name
         logger.info(f"Trying to join meeting ({team_name}). Iteration-{iteration}")
         try:
-            join_button = browser.find_element_by_css_selector(
-                "button[title='Join call with video']"
-            )
+            join_button = browser.find_element_by_xpath('//*[@id="m1631198950197"]/calling-join-button/button')
+            # join_button = browser.find_element_by_css_selector("button[title='Join call with video']")
             join_button.click()
             logger.info("Clicked on Join Button")
             return True
@@ -260,14 +263,27 @@ def alternate_driver_function():
 
         return (target_time_object - time_now).seconds
 
+    def get_time_diff(time: str) -> int:
+        time_now = dt.datetime.now()
+
+        target_time_object = dt.datetime.strptime(time, "%H:%M").replace(
+            year=time_now.year, month=time_now.month, day=time_now.day
+        )
+        
+        return abs(time_now - target_time_object).seconds
+    
+    
     for class_obj in class_list:
         logger.info(f"trying to join {class_obj.name} at {class_obj.start_time}")
         time_to_wait = get_time_to_wait(class_obj.start_time)
 
-        if time_to_wait is None:
+        if time_to_wait is None and get_time_diff(class_obj.start_time) > config.ONE_HOUR:
             logger.info(f"class {class_obj.name} is over, i guess")
 
         else:  # can wait for that time,  join class
+            if time_to_wait is None:
+                time_to_wait = 0
+            
             time_to_wait += 10  # extra 10 seconds
             logger.info(f"sleeping for {time_to_wait}s.")
             time.sleep(time_to_wait)
